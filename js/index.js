064c9b48b19dab948419f8cd2f73ebbe4f147221
Ewa Mikuła <ewamariamikula@gmail.com>
2018-07-13 22:01:33 +0200

    Add reset

--- js/index.js
+++ js/index.js
@@ -10,3 +10,15 @@ var flkty = new Flickity( elem, {
 var flkty = new Flickity( '.main-carousel', {
   // options
 });
+
+var buttonReset = document.querySelector('.reset');
+buttonReset = fizzyUIUtils.makeArray( buttons );
+
+buttonReset.addEventListener( 'click', function( event ) {
+  // filter for button clicks
+  if ( !matchesSelector( event.target, '.reset' ) ) {
+    return;
+  }
+  var index = buttons.indexOf( event.target );
+  flkty.select( index );
+});
