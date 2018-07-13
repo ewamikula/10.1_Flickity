20198d52b9136c98fdf485e97841012c6efe7dd9
Ewa Mikuła <ewamariamikula@gmail.com>
2018-07-13 22:07:52 +0200

    Add progress-bar

--- js/index.js
+++ js/index.js
@@ -22,3 +22,10 @@ buttonReset.addEventListener( 'click', function( event ) {
   var index = buttons.indexOf( event.target );
   flkty.select( index );
 });
+
+var progressBar = document.querySelector('.progress-bar')
+
+flkty.on( 'scroll', function( progress ) {
+  progress = Math.max( 0, Math.min( 1, progress ) );
+  progressBar.style.width = progress * 100 + '%';
+});
