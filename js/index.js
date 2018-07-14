2d2b276581cb0aa8f5771bf1f9bca3311226532d
Ewa Mikuła <ewamariamikula@gmail.com>
2018-07-14 23:46:40 +0200

    Add working map

--- js/index.js
+++ js/index.js
@@ -49,12 +49,23 @@ flkty.on('scroll', function(progress) {
     progressBar.style.width = progress * 100 + '%';
 });
 
-window.initMap = function() {
-  // The location of Uluru
-  var uluru = {lat: -25.344, lng: 131.036};
-  // The map, centered at Uluru
-  var map = new google.maps.Map(
-      document.getElementById('map'), {zoom: 4, center: uluru});
-  // The marker, positioned at Uluru
-  var marker = new google.maps.Marker({position: uluru, map: map});
-  }
+(function(){ 
+    
+    window.initMap = function() {
+        
+        var uluru = {lat: -25.363, lng: 131.044};
+        
+        var map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 4,
+            center: uluru
+        });
+        
+        var marker = new google.maps.Marker({
+            position: uluru,
+            map: map
+        }); 
+    }   
+     
+})();  
+
+initMap();
