dfc447134346d0c403d26652a28b53dbf45be611
Ewa Mikuła <ewamariamikula@gmail.com>
2018-07-14 22:24:12 +0200

    install mustache

--- js/index.js
+++ js/index.js
@@ -1,32 +1,53 @@
+'use strict';
+ 
+(function() {
+ 
+    var templateList = document.getElementById('template-list').innerHTML;
+    var templateItem = document.getElementById('template-slide').innerHTML;
+    Mustache.parse(templateItem);
+ 
+    var listItems = '';
+ 
+    for (var i = 0; i < slides.length; i++) {
+        console.log(slides);
+        listItems += Mustache.render(templateItem, slidesData[i]);
+    }
+ 
+    var fullProductList = Mustache.render(templateList, { slides: listItems });
+    slider.insertAdjacentHTML('beforeend', fullProductList);
+ 
+ 
+})();
+ 
 var elem = document.querySelector('.main-carousel');
-var flkty = new Flickity( elem, {
-  // options
-  cellAlign: 'left',
-  contain: true
+var flkty = new Flickity(elem, {
+    // options
+    cellAlign: 'left',
+    contain: true,
+ 
 });
-
-// element argument can be a selector string
-//   for an individual element
-var flkty = new Flickity( '.main-carousel', {
-  // options
-});
-
+ 
+ 
+ 
+ 
 var buttonGroup = document.querySelector('.button-group');
 var buttons = buttonGroup.querySelectorAll('.button');
-buttons = fizzyUIUtils.makeArray( buttons );
-
-buttonGroup.addEventListener( 'click', function( event ) {
-  // filter for button clicks
-  if ( !matchesSelector( event.target, '.button' ) ) {
-    return;
-  }
-  var index = buttons.indexOf( event.target );
-  flkty.select( index );
+buttons = fizzyUIUtils.makeArray(buttons);
+ 
+buttonGroup.addEventListener('click', function(event) {
+    // filter for button clicks
+    if (!matchesSelector(event.target, '.button')) {
+        return;
+    }
+    var index = buttons.indexOf(event.target);
+    flkty.selectCell(index);
 });
-
+ 
+ 
+ 
 var progressBar = document.querySelector('.progress-bar')
-
-flkty.on( 'scroll', function( progress ) {
-  progress = Math.max( 0, Math.min( 1, progress ) );
-  progressBar.style.width = progress * 100 + '%';
+ 
+flkty.on('scroll', function(progress) {
+    progress = Math.max(0, Math.min(1, progress));
+    progressBar.style.width = progress * 100 + '%';
 });
