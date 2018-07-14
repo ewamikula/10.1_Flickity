4638086682d2e879170543b775985277fc9381c7
Ewa Mikuła <ewamariamikula@gmail.com>
2018-07-14 23:53:27 +0200

    Add markers

--- js/index.js
+++ js/index.js
@@ -51,21 +51,46 @@ flkty.on('scroll', function(progress) {
 
 (function(){ 
     
+    var infos = document.getElementById('infos');
+
     window.initMap = function() {
         
         var uluru = {lat: -25.363, lng: 131.044};
+        var coords2 = {lat: -25.363, lng: 134.044};
+        var coords3 = {lat: -25.363, lng: 137.044};
         
         var map = new google.maps.Map(document.getElementById('map'), {
             zoom: 4,
             center: uluru
         });
         
-        var marker = new google.maps.Marker({
+            var markerOne = new google.maps.Marker({
             position: uluru,
             map: map
-        }); 
-    }   
-     
+        });
+        
+        markerOne.addListener('click', function(){
+            infos.innerHTML = 'You clicked markerOne';
+        });     
+                
+        var markerTwo = new google.maps.Marker({
+            position: coords2,
+            map: map
+        });
+
+        markerTwo.addListener('click', function(){
+            infos.innerHTML = 'You clicked markerTwo';
+        });     
+        
+        var markerThree = new google.maps.Marker({
+            position: coords3,
+            map: map
+        });
+        
+        markerThree.addListener('click', function(){
+            infos.innerHTML = 'You clicked markerThree';
+        });         
+    };
 })();  
 
 initMap();
